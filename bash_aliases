1462589daa071bf69f954df2affe42e637c35a18
Michal Lenc <michallenc@seznam.cz>
2025-05-13 21:23:16 +0200

    config update, install script
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- bash_aliases
+++ bash_aliases
@@ -1,13 +1,10 @@
 alias python=python3
 alias nv=nvim
 alias ndev='nix develop'
+alias glog='git log --oneline'
 
 if [ -f /usr/local/bin/gitbmerge ]; then
     . /usr/local/bin/gitbmerge
-fi
-
-if [ -f /usr/local/bin/glog ]; then
-    . /usr/local/bin/glog
 fi
 
 if [ -f /usr/local/bin/mmake ]; then
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s $(pwd -P)/gitignore ~/.gitignore
+ln -s $(pwd -P)/bash_aliases ~/.bash_aliases
+ln -s $(pwd -P)/gitconfig ~/.gitconfig
+sudo ln -s $(pwd -P)/scripts/gitbmerge /usr/local/bin/gitbmerge
+sudo ln -s $(pwd -P)/scripts/mmake /usr/local/bin/mmake
+ln -s $(pwd -P)/config/kitty ~/.config/kitty
+ln -s $(pwd -P)/config/nvim ~/.config/nvim
+
