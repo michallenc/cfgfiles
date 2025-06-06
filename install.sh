644e43ce4b9a8152c95bb77e564471b9e412e274
Michal Lenc <michallenc@seznam.cz>
2025-06-06 23:01:21 +0200

    config: clangd
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ sudo ln -s $(pwd -P)/scripts/gitbmerge /usr/local/bin/gitbmerge
 sudo ln -s $(pwd -P)/scripts/mmake /usr/local/bin/mmake
 ln -s $(pwd -P)/config/kitty ~/.config/kitty
 ln -s $(pwd -P)/config/nvim ~/.config/nvim
+ln -s $(pwd -P)/config/clangd ~/.config/clangd
 lb -s $(pwd -P)/nix.conf /etc/nix/nix.conf
 
